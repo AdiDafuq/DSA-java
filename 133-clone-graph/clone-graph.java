2cfa549eaf811210e69b4e8387e35140abb16477
AdiDafuq <adityasinghk14@gmail.com>
2026-05-17 09:17:54 +0530

    Time: 25 ms (79.81%) | Memory: 44.2 MB (57.85%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+
+    HashMap<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+
+        if (node == null) {
+            return null;
+        }
+
+        // Already cloned
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        // Create clone
+        Node clone = new Node(node.val);
+
+        map.put(node, clone);
+
+        // Clone neighbors
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return clone;
+    }
+}
